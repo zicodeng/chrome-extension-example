c22f6582340a58b280a750a9570329cb46a81de8
Zico Deng <zicodeng@gmail.com>
2018-02-25 14:53:38 -0800

    Fix new window not popup

--- wikit/event-page.js
+++ wikit/event-page.js
@@ -22,8 +22,8 @@ chrome.contextMenus.onClicked.addListener(function(info) {
             type: 'popup',
             top: 5,
             left: 5,
-            width: screen.availWidth / 2,
-            height: screen.availHeight / 2
+            width: Math.floor(screen.availWidth / 2),
+            height: Math.floor(screen.availHeight / 2)
         };
         chrome.windows.create(createData, function() {});
     }
