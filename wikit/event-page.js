4832dc941c2d87bf419ef45164024fe497c2729b
Zico Deng <zicodeng@gmail.com>
2018-02-25 13:44:21 -0800

    Add Wikit extension

---
+++ wikit/event-page.js
@@ -0,0 +1,30 @@
+var menuItem = {
+    id: 'wikit',
+    title: 'Wikit',
+    contexts: ['selection']
+};
+
+chrome.contextMenus.create(menuItem);
+
+function fixedEncodeURI(str) {
+    return encodeURI(str)
+        .replace(/%5B/g, '[')
+        .replace(/%5D/g, ']');
+}
+
+chrome.contextMenus.onClicked.addListener(function(info) {
+    if (info.menuItemId === 'wikit' && info.selectionText) {
+        var wikiURL =
+            'https://en.wikipedia.org/wiki/' +
+            fixedEncodeURI(info.selectionText);
+        var createData = {
+            url: wikiURL,
+            type: 'popup',
+            top: 5,
+            left: 5,
+            width: screen.availWidth / 2,
+            height: screen.availHeight / 2
+        };
+        chrome.windows.create(createData, function() {});
+    }
+});
